c0ce3bbd36301a7d713a42bdceb8472b0cb25a09
Zeblote <zeblote@gmail.com>
2026-03-24 00:29:30 +0100

    [BR] Better solver linear velocity clamp

--- physx/source/lowleveldynamics/src/DyBodyCoreIntegrator.h
+++ physx/source/lowleveldynamics/src/DyBodyCoreIntegrator.h
@@ -127,11 +127,18 @@ PX_FORCE_INLINE void integrateCore(PxVec3& motionLinearVelocity, PxVec3& motionA
 		PxVec3 linearMotionVel = solverBodyData.linearVelocity + motionLinearVelocity;
 
 		// Perform a post-solver safety clamp, mirroring the angular velocity clamp below
-		const PxReal maxV = 1e+7f;
-		const PxReal linVelSq = linearMotionVel.magnitudeSquared();
-		if (linVelSq > maxV * maxV)
+		if (!linearMotionVel.isFinite())
 		{
-			linearMotionVel *= maxV / PxSqrt(linVelSq);
+			linearMotionVel = PxVec3(0.f);
+		}
+		else
+		{
+			const PxReal maxV = 1e+5f;
+			const PxReal linVelSq = linearMotionVel.magnitudeSquared();
+			if (linVelSq > maxV * maxV)
+			{
+				linearMotionVel *= maxV / PxSqrt(linVelSq);
+			}
 		}
 
 		motionLinearVelocity = linearMotionVel;
@@ -143,6 +150,12 @@ PX_FORCE_INLINE void integrateCore(PxVec3& motionLinearVelocity, PxVec3& motionA
 
 	{
 		PxVec3 angularMotionVel = solverBodyData.angularVelocity + solverBodyData.sqrtInvInertia * motionAngularVelocity;
+
+		if (!angularMotionVel.isFinite())
+		{
+			angularMotionVel = PxVec3(0.f);
+		}
+
 		PxReal w = angularMotionVel.magnitudeSquared();
 
 		// Integrate the rotation using closed form quaternion integrator
@@ -152,7 +165,7 @@ PX_FORCE_INLINE void integrateCore(PxVec3& motionLinearVelocity, PxVec3& motionA
 			// Perform a post-solver clamping
 			// TODO(dsequeira): ignore this for the moment
 			//just clamp motionVel to half float-range
-			const PxReal maxW = 1e+7f;		//Should be about sqrt(PX_MAX_REAL/2) or smaller
+			const PxReal maxW = 1e+5f;
 			if (w > maxW)
 			{
 				angularMotionVel = angularMotionVel.getNormalized() * maxW;
