da8c7421a1b5b5ed72922d6fdbb92a88a8cf500d
Zeblote <zeblote@gmail.com>
2026-03-27 01:29:42 +0100

    [BR] Remove manifold clear hack

--- physx/source/lowlevel/common/src/pipeline/PxcNpBatch.cpp
+++ physx/source/lowlevel/common/src/pipeline/PxcNpBatch.cpp
@@ -455,16 +455,6 @@ static PX_FORCE_INLINE void discreteNarrowPhase(PxcNpThreadContext& context, con
 		const PxcContactMethod conMethod = g_PCMContactMethodTable[type0][type1];
 		PX_ASSERT(conMethod);
 
-		// BR: Something wrong with convex manifold reuse. Disabling for now.
-		if (cache.isMultiManifold())
-		{
-			cache.getMultipleManifold().clearManifold();
-		}
-		else if (cache.isManifold())
-		{
-			cache.getManifold().clearManifold();
-		}
-
 		conMethod(contactShape0, contactShape1, *tm0, *tm1, context.mNarrowPhaseParams, cache, context.mContactBuffer, &context.mRenderOutput);
 	}
 
