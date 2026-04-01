d370bf9bc4259140d22279d7931d873f32648e4b
Zeblote <zeblote@gmail.com>
2026-04-01 15:14:37 +0200

    [BR] Fix PCM manifolds losing corners (box edition)

--- physx/source/geomutils/src/pcm/GuPCMContactBoxConvex.cpp
+++ physx/source/geomutils/src/pcm/GuPCMContactBoxConvex.cpp
@@ -131,7 +131,9 @@ static bool generateOrProcessContactsBoxConvex(	const GjkConvex* relativeConvex,
 		//which means we should throw away the existing contacts and do full contact gen
 		const bool fullContactGen = FAllGrtr(FLoad(0.707106781f), V3Dot(localNor, output.normal)) || (manifold.mNumContacts < initialContacts);
 
-		if (fullContactGen || doOverlapTest)
+		// BR: GJK produces a single deepest-penetration point (face center for parallel faces),
+		// which is useless if the manifold already has >1 contact. Force full SAT regen.
+		if (fullContactGen || doOverlapTest || initialContacts > 1)
 		{
 			return fullContactsGenerationBoxConvex(relativeConvex, localConvex, transf0, transf1, manifoldContacts, contactBuffer,
 				manifold, output.normal, output.closestA, output.closestB, contactDist, idtScale, doOverlapTest, renderOutput, toleranceLength);
